d33122f22c023bb8aadbc25378c24ba57ce87c09
Shaolian <jonathan_roy_1@hotmail.com>
2013-09-27 09:35:07 -0400

    Updated Grid & Frame

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -1,5 +1,46 @@
 package model;
 
 public class Grid {
-
+	private int[][] gameGrid; 
+	
+	private static final int DEFAULT_ROW = 6;
+	private static final int DEFAULT_COLUMN = 6;
+	private static final int DEFAULT_NUMBER_TO_ALIGN = 6;
+	private static final int PLAYER_1 = 1;
+	private static final int PLAYER_2 = 2;
+	private static final int EMPTY = 0;
+	
+	private int row;
+	private int column;
+	private int nbToAlign;
+	
+	public Grid(int _row, int _column, int _nbToAlign)
+	{
+		row = _row;
+		column = _column;
+		nbToAlign = _nbToAlign;
+		
+		gameGrid = new int[row][column];
+	}
+	
+	public Grid ()
+	{
+		row = DEFAULT_ROW;
+		column = DEFAULT_COLUMN;
+		nbToAlign = DEFAULT_NUMBER_TO_ALIGN;
+		
+		gameGrid = new int[row][column];
+	}
+	
+	public void placeToken(int _player, int _column)
+	{
+		for (int i = 0; i <= row; i ++)
+		{
+			if (i == row || gameGrid[i + 1][_column] != EMPTY)
+			{
+				gameGrid[i][_column] = _player;
+			}
+		}
+	}
+	
 }
--- src/view/Frame.java
+++ src/view/Frame.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JMenuItem;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Frame extends JFrame {
 
@@ -32,11 +37,24 @@ public class Frame extends JFrame {
 	 */
 	public Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 505);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		JMenuItem mntmGiveUp = new JMenuItem("Give Up");
+		mntmGiveUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				//Make a pop-up "Are you sure?" then if said yes give-up
+			}
+		});
+		toolBar.add(mntmGiveUp);
 	}
 
 }
