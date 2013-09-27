cee49563acb07d5f92b659773b67257997316c44
Chapeau <francois.drouinm@gmail.com>
2013-09-27 10:02:46 -0400

    Solving conflicts

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -1,5 +1,70 @@
 package model;
 
 public class Grid {
-
+	private int[][] gameGrid; 
+	
+	private static final int DEFAULT_ROW = 6;
+	private static final int DEFAULT_COLUMN = 6;
+	private static final int DEFAULT_NUMBER_TO_ALIGN = 6;
+	private static final int PLAYER_1 = 1;
+	private static final int PLAYER_2 = 2;
+	private static final int EMPTY = 0;
+	
+	private int row;
+	private int column;
+	private int nbToAlign;
+	
+	public Grid(int _row, int _column, int _nbToAlign)
+	{
+		row = _row;
+		column = _column;
+		nbToAlign = _nbToAlign;
+		
+		gameGrid = new int[row][column];
+	}
+	
+	public Grid ()
+	{
+		row = DEFAULT_ROW;
+		column = DEFAULT_COLUMN;
+		nbToAlign = DEFAULT_NUMBER_TO_ALIGN;
+		
+		gameGrid = new int[row][column];
+	}
+	
+	public boolean placeToken(int _player, int _column)
+	{
+		if(checkIfValidColumn(_column) == true)
+		{
+			for (int i = 0; i < row; i ++)
+			{
+				if (i == row - 1)
+				{
+					gameGrid[i][_column - 1] = _player;
+					return true;
+				}
+				else if (gameGrid[i + 1][_column - 1] != EMPTY)
+				{
+					gameGrid[i][_column - 1] = _player;
+					return true;
+				}
+			}
+			
+		}
+		return false;
+	}
+	
+	private boolean checkIfValidColumn(int _column)
+	{
+		boolean valid = true;
+		
+		if (gameGrid[row - 1][_column - 1] != EMPTY || _column > column)
+		{
+			valid = false;
+		}
+		
+		return valid;
+				
+	}
+	
 }
--- src/view/Frame.java
+++ src/view/Frame.java
@@ -6,7 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+<<<<<<< HEAD
 import javax.swing.JButton;
+=======
+import javax.swing.JToolBar;
+import javax.swing.JMenuItem;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+>>>>>>> 28153aa7a9bf7681fe78b3107f7addfc24509c1d
 
 public class Frame extends JFrame {
 
@@ -36,17 +44,32 @@ public class Frame extends JFrame {
 	public Frame() {
 		setTitle("Test");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 505);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
+<<<<<<< HEAD
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.NORTH);
 		
 		JButton btnNewButton = new JButton("New button");
 		panel.add(btnNewButton);
+=======
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		JMenuItem mntmGiveUp = new JMenuItem("Give Up");
+		mntmGiveUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				//Make a pop-up "Are you sure?" then if said yes give-up
+			}
+		});
+		toolBar.add(mntmGiveUp);
+>>>>>>> 28153aa7a9bf7681fe78b3107f7addfc24509c1d
 	}
 
 }
